cae1adb6a24406ccf98d3f24444912d4a0df61ce
mpenajoia <penajoia.marco@gmail.com>
2022-03-21 21:47:02 -0700

    tech comp

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -4,9 +4,19 @@ import { UserContext } from '../context/UserContext';
 function Tech() {
   const { info } = useContext(UserContext);
   const tech = info.tech;
-  const techMap = tech.tech.map((tech, key) => {
+  const typeMap = tech.tech.map((tech, key) => {
     return (
-      
+      <p key={key} className='font-medium leading-5 '>{tech.type}</p>
+    )
+  })
+  const listMap = tech.tech.map((item, key) => {
+    const eachItem = item.list.map((each, key) => {
+      return (
+        <span key={key} className="mr-2" >{each}</span>
+      )
+    })
+    return (
+      <p key={key} className='text-sm'>{eachItem}</p>
     )
   })
 
@@ -15,18 +25,20 @@ function Tech() {
       <h2 className='text-yellow-400 font-bold text-xl'>Tech Stack</h2>
       <div className='flex w-full justify-between'>
         <div className='flex flex-col '>
-          <p className='font-medium leading-5 '>Languages</p>
+          {typeMap}
+          {/* <p className='font-medium leading-5 '>Languages</p>
           <p className='font-medium leading-5 '>Framework | Library</p>
           <p className='font-medium leading-5 '>Database | Platform</p>
           <p className='font-medium leading-5 '>Tools | Markdown</p>
-          <p className='font-medium leading-5 '>Misc</p>
+          <p className='font-medium leading-5 '>Misc</p> */}
         </div>
         <div className='flex flex-col '>
-          <p className='text-sm'>JavaScript ES6 | Python</p>
+          {listMap}
+          {/* <p className='text-sm'>JavaScript ES6 | Python</p>
           <p className='text-sm'>React | Node | Express | Tailwind | Bootstrap</p>
           <p className='text-sm'>MongoDB | PostgreSQL | RESTful API's</p>
           <p className='text-sm'>Git | HTML | CSS | Markup</p>
-          <p className='text-sm'>Photoshop | Adobe Creative Suite | Google Suite | Zoom</p>
+          <p className='text-sm'>Photoshop | Adobe Creative Suite | Google Suite | Zoom</p> */}
         </div>
       </div>
     </div>
